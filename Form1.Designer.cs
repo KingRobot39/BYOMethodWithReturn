780d6d4350cc14f7bc077dfa70ae1c3e9ee0ead7
Dylan Hughes <hughesdylan39@outlook.com>
2019-09-18 15:17:55 -0400

    third commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace carcostcomparison___dylan_hughes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.yearlabel = new System.Windows.Forms.Label();
+            this.makelabel = new System.Windows.Forms.Label();
+            this.modellabel = new System.Windows.Forms.Label();
+            this.mpglabel = new System.Windows.Forms.Label();
+            this.purchasepricelabel = new System.Windows.Forms.Label();
+            this.repairlabel = new System.Windows.Forms.Label();
+            this.insurancecostlabel = new System.Windows.Forms.Label();
+            this.fuelcostlabel = new System.Windows.Forms.Label();
+            this.allcostlabel = new System.Windows.Forms.Label();
+            this.car1label = new System.Windows.Forms.Label();
+            this.car2label = new System.Windows.Forms.Label();
+            this.yearc1box = new System.Windows.Forms.TextBox();
+            this.makec1box = new System.Windows.Forms.TextBox();
+            this.modelc1box = new System.Windows.Forms.TextBox();
+            this.mpgc1box = new System.Windows.Forms.TextBox();
+            this.purchasepricec1box = new System.Windows.Forms.TextBox();
+            this.repaircostc1box = new System.Windows.Forms.TextBox();
+            this.insurancecostc1box = new System.Windows.Forms.TextBox();
+            this.yearc2box = new System.Windows.Forms.TextBox();
+            this.makec2box = new System.Windows.Forms.TextBox();
+            this.modelc2box = new System.Windows.Forms.TextBox();
+            this.mpgc2box = new System.Windows.Forms.TextBox();
+            this.purchasepricec2box = new System.Windows.Forms.TextBox();
+            this.repaircostc2box = new System.Windows.Forms.TextBox();
+            this.insurancecostc2box = new System.Windows.Forms.TextBox();
+            this.c1fuelcostlabel = new System.Windows.Forms.Label();
+            this.c2fuelcostlabel = new System.Windows.Forms.Label();
+            this.c1allcostlabel = new System.Windows.Forms.Label();
+            this.c2allcostlabel = new System.Windows.Forms.Label();
+            this.calculatebutton = new System.Windows.Forms.Button();
+            this.clearbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // yearlabel
+            // 
+            this.yearlabel.Location = new System.Drawing.Point(37, 45);
+            this.yearlabel.Name = "yearlabel";
+            this.yearlabel.Size = new System.Drawing.Size(100, 23);
+            this.yearlabel.TabIndex = 0;
+            this.yearlabel.Text = "Year Manufactured";
+            // 
+            // makelabel
+            // 
+            this.makelabel.Location = new System.Drawing.Point(40, 78);
+            this.makelabel.Name = "makelabel";
+            this.makelabel.Size = new System.Drawing.Size(100, 23);
+            this.makelabel.TabIndex = 1;
+            this.makelabel.Text = "Make";
+            // 
+            // modellabel
+            // 
+            this.modellabel.Location = new System.Drawing.Point(40, 101);
+            this.modellabel.Name = "modellabel";
+            this.modellabel.Size = new System.Drawing.Size(100, 23);
+            this.modellabel.TabIndex = 2;
+            this.modellabel.Text = "Model Name";
+            // 
+            // mpglabel
+            // 
+            this.mpglabel.Location = new System.Drawing.Point(40, 135);
+            this.mpglabel.Name = "mpglabel";
+            this.mpglabel.Size = new System.Drawing.Size(116, 38);
+            this.mpglabel.TabIndex = 3;
+            this.mpglabel.Text = "Published Miles Per Gallon MPG";
+            // 
+            // purchasepricelabel
+            // 
+            this.purchasepricelabel.Location = new System.Drawing.Point(40, 192);
+            this.purchasepricelabel.Name = "purchasepricelabel";
+            this.purchasepricelabel.Size = new System.Drawing.Size(100, 32);
+            this.purchasepricelabel.TabIndex = 4;
+            this.purchasepricelabel.Text = "Initial Purchase Price";
+            // 
+            // repairlabel
+            // 
+            this.repairlabel.Location = new System.Drawing.Point(37, 233);
+            this.repairlabel.Name = "repairlabel";
+            this.repairlabel.Size = new System.Drawing.Size(100, 23);
+            this.repairlabel.TabIndex = 5;
+            this.repairlabel.Text = "Annual Repair Cost";
+            // 
+            // insurancecostlabel
+            // 
+            this.insurancecostlabel.Location = new System.Drawing.Point(40, 265);
+            this.insurancecostlabel.Name = "insurancecostlabel";
+            this.insurancecostlabel.Size = new System.Drawing.Size(100, 26);
+            this.insurancecostlabel.TabIndex = 6;
+            this.insurancecostlabel.Text = "Annual Insurance Cost";
+            // 
+            // fuelcostlabel
+            // 
+            this.fuelcostlabel.Location = new System.Drawing.Point(37, 333);
+            this.fuelcostlabel.Name = "fuelcostlabel";
+            this.fuelcostlabel.Size = new System.Drawing.Size(100, 32);
+            this.fuelcostlabel.TabIndex = 7;
+            this.fuelcostlabel.Text = "Calculated 1 Year Total FUEL Cost";
+            // 
+            // allcostlabel
+            // 
+            this.allcostlabel.Location = new System.Drawing.Point(37, 378);
+            this.allcostlabel.Name = "allcostlabel";
+            this.allcostlabel.Size = new System.Drawing.Size(100, 30);
+            this.allcostlabel.TabIndex = 8;
+            this.allcostlabel.Text = "Calculated 5 Year Total of ALL Costs";
+            // 
+            // car1label
+            // 
+            this.car1label.AutoSize = true;
+            this.car1label.Location = new System.Drawing.Point(188, 13);
+            this.car1label.Name = "car1label";
+            this.car1label.Size = new System.Drawing.Size(32, 13);
+            this.car1label.TabIndex = 9;
+            this.car1label.Text = "Car 1";
+            // 
+            // car2label
+            // 
+            this.car2label.AutoSize = true;
+            this.car2label.Location = new System.Drawing.Point(333, 13);
+            this.car2label.Name = "car2label";
+            this.car2label.Size = new System.Drawing.Size(32, 13);
+            this.car2label.TabIndex = 10;
+            this.car2label.Text = "Car 2";
+            // 
+            // yearc1box
+            // 
+            this.yearc1box.Location = new System.Drawing.Point(191, 42);
+            this.yearc1box.Name = "yearc1box";
+            this.yearc1box.Size = new System.Drawing.Size(100, 20);
+            this.yearc1box.TabIndex = 11;
+            // 
+            // makec1box
+            // 
+            this.makec1box.Location = new System.Drawing.Point(191, 69);
+            this.makec1box.Name = "makec1box";
+            this.makec1box.Size = new System.Drawing.Size(100, 20);
+            this.makec1box.TabIndex = 12;
+            // 
+            // modelc1box
+            // 
+            this.modelc1box.Location = new System.Drawing.Point(191, 96);
+            this.modelc1box.Name = "modelc1box";
+            this.modelc1box.Size = new System.Drawing.Size(100, 20);
+            this.modelc1box.TabIndex = 13;
+            // 
+            // mpgc1box
+            // 
+            this.mpgc1box.Location = new System.Drawing.Point(191, 135);
+            this.mpgc1box.Name = "mpgc1box";
+            this.mpgc1box.Size = new System.Drawing.Size(100, 20);
+            this.mpgc1box.TabIndex = 14;
+            // 
+            // purchasepricec1box
+            // 
+            this.purchasepricec1box.Location = new System.Drawing.Point(191, 192);
+            this.purchasepricec1box.Name = "purchasepricec1box";
+            this.purchasepricec1box.Size = new System.Drawing.Size(100, 20);
+            this.purchasepricec1box.TabIndex = 15;
+            // 
+            // repaircostc1box
+            // 
+            this.repaircostc1box.Location = new System.Drawing.Point(191, 233);
+            this.repaircostc1box.Name = "repaircostc1box";
+            this.repaircostc1box.Size = new System.Drawing.Size(100, 20);
+            this.repaircostc1box.TabIndex = 16;
+            // 
+            // insurancecostc1box
+            // 
+            this.insurancecostc1box.Location = new System.Drawing.Point(191, 265);
+            this.insurancecostc1box.Name = "insurancecostc1box";
+            this.insurancecostc1box.Size = new System.Drawing.Size(100, 20);
+            this.insurancecostc1box.TabIndex = 17;
+            // 
+            // yearc2box
+            // 
+            this.yearc2box.Location = new System.Drawing.Point(336, 42);
+            this.yearc2box.Name = "yearc2box";
+            this.yearc2box.Size = new System.Drawing.Size(100, 20);
+            this.yearc2box.TabIndex = 18;
+            // 
+            // makec2box
+            // 
+            this.makec2box.Location = new System.Drawing.Point(336, 68);
+            this.makec2box.Name = "makec2box";
+            this.makec2box.Size = new System.Drawing.Size(100, 20);
+            this.makec2box.TabIndex = 19;
+            // 
+            // modelc2box
+            // 
+            this.modelc2box.Location = new System.Drawing.Point(336, 96);
+            this.modelc2box.Name = "modelc2box";
+            this.modelc2box.Size = new System.Drawing.Size(100, 20);
+            this.modelc2box.TabIndex = 20;
+            // 
+            // mpgc2box
+            // 
+            this.mpgc2box.Location = new System.Drawing.Point(336, 135);
+            this.mpgc2box.Name = "mpgc2box";
+            this.mpgc2box.Size = new System.Drawing.Size(100, 20);
+            this.mpgc2box.TabIndex = 21;
+            // 
+            // purchasepricec2box
+            // 
+            this.purchasepricec2box.Location = new System.Drawing.Point(336, 192);
+            this.purchasepricec2box.Name = "purchasepricec2box";
+            this.purchasepricec2box.Size = new System.Drawing.Size(100, 20);
+            this.purchasepricec2box.TabIndex = 22;
+            // 
+            // repaircostc2box
+            // 
+            this.repaircostc2box.Location = new System.Drawing.Point(336, 233);
+            this.repaircostc2box.Name = "repaircostc2box";
+            this.repaircostc2box.Size = new System.Drawing.Size(100, 20);
+            this.repaircostc2box.TabIndex = 23;
+            // 
+            // insurancecostc2box
+            // 
+            this.insurancecostc2box.Location = new System.Drawing.Point(336, 264);
+            this.insurancecostc2box.Name = "insurancecostc2box";
+            this.insurancecostc2box.Size = new System.Drawing.Size(100, 20);
+            this.insurancecostc2box.TabIndex = 24;
+            // 
+            // c1fuelcostlabel
+            // 
+            this.c1fuelcostlabel.BackColor = System.Drawing.SystemColors.Control;
+            this.c1fuelcostlabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.c1fuelcostlabel.Location = new System.Drawing.Point(191, 333);
+            this.c1fuelcostlabel.Name = "c1fuelcostlabel";
+            this.c1fuelcostlabel.Size = new System.Drawing.Size(100, 32);
+            this.c1fuelcostlabel.TabIndex = 25;
+            // 
+            // c2fuelcostlabel
+            // 
+            this.c2fuelcostlabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.c2fuelcostlabel.Location = new System.Drawing.Point(336, 333);
+            this.c2fuelcostlabel.Name = "c2fuelcostlabel";
+            this.c2fuelcostlabel.Size = new System.Drawing.Size(100, 32);
+            this.c2fuelcostlabel.TabIndex = 26;
+            // 
+            // c1allcostlabel
+            // 
+            this.c1allcostlabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.c1allcostlabel.Location = new System.Drawing.Point(191, 378);
+            this.c1allcostlabel.Name = "c1allcostlabel";
+            this.c1allcostlabel.Size = new System.Drawing.Size(100, 30);
+            this.c1allcostlabel.TabIndex = 27;
+            // 
+            // c2allcostlabel
+            // 
+            this.c2allcostlabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.c2allcostlabel.Location = new System.Drawing.Point(336, 377);
+            this.c2allcostlabel.Name = "c2allcostlabel";
+            this.c2allcostlabel.Size = new System.Drawing.Size(100, 31);
+            this.c2allcostlabel.TabIndex = 28;
+            // 
+            // calculatebutton
+            // 
+            this.calculatebutton.Location = new System.Drawing.Point(216, 291);
+            this.calculatebutton.Name = "calculatebutton";
+            this.calculatebutton.Size = new System.Drawing.Size(75, 23);
+            this.calculatebutton.TabIndex = 29;
+            this.calculatebutton.Text = "Calculate";
+            this.calculatebutton.UseVisualStyleBackColor = true;
+            this.calculatebutton.Click += new System.EventHandler(this.calculatebutton_Click);
+            // 
+            // clearbutton
+            // 
+            this.clearbutton.Location = new System.Drawing.Point(336, 291);
+            this.clearbutton.Name = "clearbutton";
+            this.clearbutton.Size = new System.Drawing.Size(75, 23);
+            this.clearbutton.TabIndex = 30;
+            this.clearbutton.Text = "Clear";
+            this.clearbutton.UseVisualStyleBackColor = true;
+            this.clearbutton.Click += new System.EventHandler(this.clearbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 450);
+            this.Controls.Add(this.clearbutton);
+            this.Controls.Add(this.calculatebutton);
+            this.Controls.Add(this.c2allcostlabel);
+            this.Controls.Add(this.c1allcostlabel);
+            this.Controls.Add(this.c2fuelcostlabel);
+            this.Controls.Add(this.c1fuelcostlabel);
+            this.Controls.Add(this.insurancecostc2box);
+            this.Controls.Add(this.repaircostc2box);
+            this.Controls.Add(this.purchasepricec2box);
+            this.Controls.Add(this.mpgc2box);
+            this.Controls.Add(this.modelc2box);
+            this.Controls.Add(this.makec2box);
+            this.Controls.Add(this.yearc2box);
+            this.Controls.Add(this.insurancecostc1box);
+            this.Controls.Add(this.repaircostc1box);
+            this.Controls.Add(this.purchasepricec1box);
+            this.Controls.Add(this.mpgc1box);
+            this.Controls.Add(this.modelc1box);
+            this.Controls.Add(this.makec1box);
+            this.Controls.Add(this.yearc1box);
+            this.Controls.Add(this.car2label);
+            this.Controls.Add(this.car1label);
+            this.Controls.Add(this.allcostlabel);
+            this.Controls.Add(this.fuelcostlabel);
+            this.Controls.Add(this.insurancecostlabel);
+            this.Controls.Add(this.repairlabel);
+            this.Controls.Add(this.purchasepricelabel);
+            this.Controls.Add(this.mpglabel);
+            this.Controls.Add(this.modellabel);
+            this.Controls.Add(this.makelabel);
+            this.Controls.Add(this.yearlabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label yearlabel;
+        private System.Windows.Forms.Label makelabel;
+        private System.Windows.Forms.Label modellabel;
+        private System.Windows.Forms.Label mpglabel;
+        private System.Windows.Forms.Label purchasepricelabel;
+        private System.Windows.Forms.Label repairlabel;
+        private System.Windows.Forms.Label insurancecostlabel;
+        private System.Windows.Forms.Label fuelcostlabel;
+        private System.Windows.Forms.Label allcostlabel;
+        private System.Windows.Forms.Label car1label;
+        private System.Windows.Forms.Label car2label;
+        private System.Windows.Forms.TextBox yearc1box;
+        private System.Windows.Forms.TextBox makec1box;
+        private System.Windows.Forms.TextBox modelc1box;
+        private System.Windows.Forms.TextBox mpgc1box;
+        private System.Windows.Forms.TextBox purchasepricec1box;
+        private System.Windows.Forms.TextBox repaircostc1box;
+        private System.Windows.Forms.TextBox insurancecostc1box;
+        private System.Windows.Forms.TextBox yearc2box;
+        private System.Windows.Forms.TextBox makec2box;
+        private System.Windows.Forms.TextBox modelc2box;
+        private System.Windows.Forms.TextBox mpgc2box;
+        private System.Windows.Forms.TextBox purchasepricec2box;
+        private System.Windows.Forms.TextBox repaircostc2box;
+        private System.Windows.Forms.TextBox insurancecostc2box;
+        private System.Windows.Forms.Label c1fuelcostlabel;
+        private System.Windows.Forms.Label c2fuelcostlabel;
+        private System.Windows.Forms.Label c1allcostlabel;
+        private System.Windows.Forms.Label c2allcostlabel;
+        private System.Windows.Forms.Button calculatebutton;
+        private System.Windows.Forms.Button clearbutton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace carcostcomparison___dylan_hughes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //constants
+        //declair required constants
+        const decimal fuelconst = 2.50m;
+        const decimal milesperyearconst = 12000;
+
+        private void calculatebutton_Click(object sender, EventArgs e)
+        {
+            //input
+            //declair A LOT of variables
+            decimal mpgc1V = 0;
+            decimal mpgc2V = 0;
+            decimal totalfuelcostc1V = 0;
+            decimal totalfuelcostc2V = 0;
+            decimal purchasepricec1V = 0;
+            decimal purchasepricec2V = 0;
+            decimal repaircostc1V = 0;
+            decimal repaircostc2V = 0;
+            decimal insurancecostc1V = 0;
+            decimal insurancecostc2V = 0;
+            decimal totalcostc1V = 0;
+            decimal totalcostc2V = 0;
+
+            //try/catch to make sure numbers are entered in required textboxes
+            //parses each input
+            try
+            {
+                mpgc1V = decimal.Parse(mpgc1box.Text);
+                mpgc2V = decimal.Parse(mpgc2box.Text);
+                purchasepricec1V = decimal.Parse(purchasepricec1box.Text);
+                purchasepricec2V = decimal.Parse(purchasepricec2box.Text);
+                repaircostc1V = decimal.Parse(repaircostc1box.Text);
+                repaircostc2V = decimal.Parse(repaircostc2box.Text);
+                insurancecostc1V = decimal.Parse(insurancecostc1box.Text);
+                insurancecostc2V = decimal.Parse(insurancecostc2box.Text);
+            }
+
+            catch
+            {
+                MessageBox.Show("Please enter each cars requested information.");
+                return;
+            }
+
+            //processing
+            //formulas to calculate requested information
+            totalfuelcostc1V = dylanhughesMETHODCalc1YearFuelCost(milesperyearconst, fuelconst, mpgc1V);
+            totalfuelcostc2V = dylanhughesMETHODCalc1YearFuelCost(milesperyearconst, fuelconst, mpgc2V);
+            totalcostc1V = purchasepricec1V + 5 * (totalfuelcostc1V + repaircostc1V + insurancecostc1V);
+            totalcostc2V = purchasepricec2V + 5 * (totalfuelcostc2V + repaircostc2V + insurancecostc2V);
+
+
+            //output
+            //displays results of above calculations in the correct labels
+            c1fuelcostlabel.Text = totalfuelcostc1V.ToString("c");
+            c2fuelcostlabel.Text = totalfuelcostc2V.ToString("c");
+            c1allcostlabel.Text = totalcostc1V.ToString("c");
+            c2allcostlabel.Text = totalcostc2V.ToString("c");
+
+            //colors higher cost label red
+            if (totalcostc1V > totalcostc2V)
+            {
+                c1allcostlabel.BackColor = Color.Red;
+            }
+
+            else
+            {
+                c2allcostlabel.BackColor = Color.Red;
+            }
+        }
+
+        private void clearbutton_Click(object sender, EventArgs e)
+        {
+            //clears all textboxes
+            mpgc1box.Text = "";
+            mpgc2box.Text = "";
+            purchasepricec1box.Text = "";
+            purchasepricec2box.Text = "";
+            repaircostc1box.Text = "";
+            repaircostc2box.Text = "";
+            insurancecostc1box.Text = "";
+            insurancecostc2box.Text = "";
+            c1fuelcostlabel.Text = "";
+            c2fuelcostlabel.Text = "";
+            c1allcostlabel.Text = "";
+            c2allcostlabel.Text = "";
+
+            //reverts label colors back to default
+            c1allcostlabel.BackColor = default(Color);
+            c2allcostlabel.BackColor = default(Color);
+
+        }
+
+        private decimal dylanhughesMETHODCalc1YearFuelCost(decimal milesdriven, decimal fulecost, decimal carmpg)
+        {
+            decimal yearlyfulecostout = 0;
+
+            yearlyfulecostout = (milesdriven / carmpg) * fulecost;
+
+            return yearlyfulecostout;
+        }
+    }
+}
